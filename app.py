d36d5a03ece88a3006bd3603d1ac54544057cc0a
Lyn-Solarn <lalarson2003@gmail.com>
2025-07-25 22:41:12 -0700

    Completed CSS and connected all templates to app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import DateField
+from wtforms import DateField, HiddenField, RadioField
 from wtforms.validators import DataRequired
 from datetime import datetime, timedelta
 import requests
-# Loads variables in the .env
 from dotenv import load_dotenv
 
-# Note: Run app with 'flask --app app --debug run'
-
+# Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
@@ -19,6 +17,21 @@ bootstrap = Bootstrap5(app)
 
 NASA_API_KEY = os.getenv("NASA_API_KEY")
 
+# --- Forms ---
+class SearchAction(FlaskForm):
+    search_input = DateField('Search', validators=[DataRequired()])
+
+class ImageSave(FlaskForm):
+    image_title = HiddenField()
+    date_of_image = HiddenField()
+    decision = RadioField('Add to Favorites?',
+                          choices=[('yes', 'Yes'), ('no', 'No')],
+                          validators=[DataRequired()])
+
+# --- Favorites Store ---
+favorites_album = []
+
+# --- Helpers ---
 def fetch_apod_images():
     """
     Creates a difference of NUM_Days - 1 (i.e. if NUM_DAYS=10 then it becomes timedelta(days=4)). It then subtracts that
@@ -27,7 +40,7 @@ def fetch_apod_images():
 
     After getting the ending and starting dates, it sets up the NASA's API request and calls it.
     """
-    NUM_DAYS = 10
+    NUM_DAYS = 25
     today = datetime.today()
     start_date = (today - timedelta(days=NUM_DAYS - 1)).strftime('%Y-%m-%d')
     end_date = today.strftime('%Y-%m-%d')
@@ -40,46 +53,74 @@ def fetch_apod_images():
     }
 
     response = requests.get(url, params=params)
-
-    # Checks to see if the API call was successful
     if response.status_code == 200:
-        # Reverse to show newest first
-        return response.json()[::-1]
-    else:
-        return []
+        return response.json()[::-1]  # newest first
+    return []
 
-class SearchAction(FlaskForm):
-    search_input = DateField('Search',validators=[DataRequired()])
+def fetch_apod_by_date(date):
+    endpoint = 'https://api.nasa.gov/planetary/apod'
+    payload = {
+        'api_key': NASA_API_KEY,
+        'date': date
+    }
+    r = requests.get(endpoint, params=payload)
+    return r.json()
 
-class ImageSave(FlaskForm):
-    pass # filler method because I don't know what to do here...
-
-favorites_album = []
-
-def favorite_picture(my_image):
-    favorites_album.append(my_image)
-    
-def remove_favorite_picture(my_image):
-     my_image.remove(my_image)
-
-
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/')
+# --- Routes ---
+@app.route('/', methods=['GET', 'POST'])
 def gallery():
-	images = fetch_apod_images()
+    form = SearchAction()
+    error = None
+    images = fetch_apod_images()
 
-	form = SearchAction()
-	if form.validate_on_submit():
-		return redirect('search'+form.search_input.data)
-	return render_template('galleryview.html', form=form, images=images)
+    if form.validate_on_submit():
+        return redirect(f'/search/{form.search_input.data}')
 
-@app.route('/favorites')
+    return render_template('galleryview.html', form=form, images=images, error=error)
+
+@app.route('/search/<date>', methods=['GET', 'POST'])
+def search(date):
+    form = ImageSave()
+    data = fetch_apod_by_date(date)
+
+    if data.get('media_type') != 'image':
+        flash("That date doesn't contain an image. Try another.")
+        return redirect('/')
+
+    if form.validate_on_submit():
+        if form.decision.data == 'yes':
+            image_date = form.date_of_image.data
+            image_title = form.image_title.data
+            if image_date not in [item[0] for item in favorites_album]:
+                favorites_album.append([image_date, image_title])
+        return redirect('/favorites')
+
+    # Pre-fill form fields
+    form.date_of_image.data = data.get('date')
+    form.image_title.data = data.get('title')
+
+    return render_template('searchview.html', data=data, form=form)
+
+@app.route('/favorites', methods=['GET', 'POST'])
 def favorites():
-	form = SearchAction()
-	if form.validate_on_submit():
-		return redirect('search'+form.search_input.data)
-	return render_template('favoritesview.html', form=form, favorites_album=favorites_album)
+    if request.method == 'POST':
+        image_title = request.form.get('image_title')
+        image_date = request.form.get('date_of_image')
+        decision = request.form.get('decision')
 
+        if decision == 'yes' and image_date:
+            if image_date not in [item[0] for item in favorites_album]:
+                favorites_album.append([image_date, image_title])
+        return redirect('/')
 
+    # GET: Show current favorites
+    images = []
+    for date, _ in favorites_album:
+        data = fetch_apod_by_date(date)
+        if data.get('media_type') == 'image':
+            images.append(data)
+    return render_template('favoritesview.html', images=images)
+
+# --- Run ---
 if __name__ == "__main__":
     app.run(debug=True)
